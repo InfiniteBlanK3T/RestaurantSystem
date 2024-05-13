1293fac79df18d561887c93d2a0ab885fa944587
Thomas <vo.thomas.11647@outlook.com>
2024-05-13 13:10:32 +1000

    Emergency changes

--- RestaurantSystem/settings.py
+++ RestaurantSystem/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.auth',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,18 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'RestaurantSystem.wsgi.application'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
 
 
 # Database
---
+++ restaurant/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import MenuItem, Order, Reservation
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'name', 'description', 'price', 'image', 'restaurant']
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'restaurant', 'created_at', 'total_amount', 'status']
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = ['id', 'user', 'restaurant', 'date', 'time', 'party_size']
